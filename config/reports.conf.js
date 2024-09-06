3f864768bf55dbf87b5c3b1a3fa38ee3bfc0f93e
Josimari Fabri <153103230+JosimariFabri@users.noreply.github.com>
2024-09-06 13:13:45 -0300

    Delete

--- config/reports.conf.js
+++
@@ -1,9 +0,0 @@
-export let reportersConf = process.env.REPORTER == 'true' ? {
-    reporters: ['spec',
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false
-        }]
-    ],
-} : {}
--- config/specs.conf.js
+++
@@ -1,5 +0,0 @@
-export let specsConf = {
-    specs: [
-        '../test/specs/**/*.js'
-    ]
-}
